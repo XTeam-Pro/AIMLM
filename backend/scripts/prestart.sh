8078b6284f6d59264f1782f6c579e6a8db7f5e2f
BeefBoy <a.fokin1@yandex.com>
2025-03-22 11:54:42 +0300

    correct 'test backend'

--- backend/scripts/prestart.sh
+++ backend/scripts/prestart.sh
@@ -3,11 +3,12 @@
 set -e
 set -x
 
+# Run migrations
+alembic upgrade head
+
 # Let the DB start
 python app/backend_pre_start.py
 
-# Run migrations
-alembic upgrade head
 
 # Create initial data in DB
 python app/initial_data.py
